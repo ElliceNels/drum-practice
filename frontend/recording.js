c83785aee3f98034f999e3a590e3285b44181e7d
ElliceNels <23371323@studentmail.ul.ie>
2025-11-30 18:30:43 +0000

    [Feat] Add Live audio capture

---
+++ frontend/recording.js
@@ -0,0 +1,55 @@
+let recorder;
+let chunks = [];
+const timeSliceMs = 20;
+const MIME_WEBM = "audio/webm";
+const MIME_MP4 = "audio/mp4";
+const WEBM = "webm";
+const MP4 = "mp4";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  
+  let type = MIME_WEBM;
+  if (!MediaRecorder.isTypeSupported(type)) {
+    type = MIME_MP4;
+  }
+
+  recorder = new MediaRecorder(stream, { mimeType: type });
+  
+  document.getElementById("start-button").onclick = () => {
+    document.getElementById("status-text").innerText = "Recording...";
+    recorder.start(timeSliceMs);
+  };
+
+
+  document.getElementById("stop-button").onclick  = () => {
+    document.getElementById("status-text").innerText = "Idle";
+    recorder.stop();
+  };
+
+
+  recorder.ondataavailable = e => {
+    chunks.push(e.data);
+    // TODO: Send chunk to server
+    e.data.arrayBuffer().then(buffer => {
+      // Log chunk size for debugging
+      console.log("Chunk size (bytes):", buffer.byteLength);
+    });
+  };
+
+  recorder.onstop = () => {
+    const audioBlob = new Blob(chunks, { type });
+
+    const url = URL.createObjectURL(audioBlob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "recording." + (type.includes(WEBM) ? WEBM : MP4);
+    a.click();
+
+    // TODO: send whole file to server
+
+    // Clear chunks for next recording
+    chunks = [];
+  };
+
+});
