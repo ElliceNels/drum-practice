fe6dbd0dab3c45fee77f86108bb0cd05f995f4cc
ElliceNels <23371323@studentmail.ul.ie>
2025-12-17 11:09:29 +0000

    [Feat] Connect offline proessing to socket when file received

--- backend/server_app/socket_api/audio.py
+++ backend/server_app/socket_api/audio.py
@@ -2,16 +2,20 @@
 import logging
 from flask_socketio import Namespace, emit
 from audio_processing.online import OnlineAubioProcessor
+from audio_processing.offline import OfflineAudioProcessor
 
 logger = logging.getLogger(__name__)
 
 
 class AudioNamespace(Namespace):
     """Socket.IO namespace for handling audio streaming."""
+    PERFOMANCE_SUMMARY_EVENT = "performance_summary"
+    CHUNK_RESPONSE_EVENT = "chunk_response"
 
     def __init__(self, namespace):
         super().__init__(namespace)
         self.online_processor = OnlineAubioProcessor()
+        self.offline_processor = OfflineAudioProcessor()
 
     def on_connect(self):
         """Handle a new client connection."""
@@ -35,7 +39,14 @@ class AudioNamespace(Namespace):
     def on_receive_audio_file(self, data):
         """Handle incoming full audio file."""
         logger.info("Received audio file of size: %d", len(data))
-        # Process the audio file with online audio processing
+        # Process the audio file with offline audio processing
+        audio_data = self.offline_processor.load_audio_from_bytes(data)
+        beats, downbeats = self.offline_processor.analyze_audio(audio_data)
+        bpm_arr = self.offline_processor.calculate_bpm_array(beats)
+        rank = self.offline_processor.performance_to_rank(bpm_arr, target_bpm=self.online_processor.desired_bpm)
+        if rank:
+            logger.info("Sending performance summary: %s", rank)
+            emit("performance_summary", {"rank": rank[0], "description": rank[1]})
 
     def on_desired_tempo(self, data):
         """Handle desired tempo setting from client."""
